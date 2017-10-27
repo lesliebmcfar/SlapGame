2ba0574193b2d7a33dc853dd1d392c1a31940c8b
Your Name <Your email>
2017-10-27 14:24:17 -0600

    part one done-working!

--- game.js
+++ game.js
@@ -1,8 +1,32 @@
-var health = 100
-function slap(){
-{health-=1}
-alert(health)
+var health = 100;
+var name = "";
+var hits=0;
+
+function rock() {
+    health -= 1
+    hits += 1
+    update()
+}          
+
+
+function lightening() {
+     health -= 5 
+    hits += 1
+        update()  
+    }
+
+function lava() {
+    health -= 10 
+   hits += 1
+    update()   
 }
 
 
 
+function update() {
+    document.getElementById("health").innerText=health;
+    document.getElementById("name").innerText=name;
+    document.getElementById("hits").innerText=hits;
+}
+
+update()
